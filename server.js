03b4195f41f64cca5ed7cb4e00264044390fd687
Sanzhar Zh <snzhrski@gmail.com>
2024-03-28 19:04:57 -0500

    rules changed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const jsonServer = require('json-server');
-const auth = require('json-server-auth')
+// const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 server.db = router.db
 
 server.use(middlewares);
-server.use(auth);
+// server.use(auth);
 server.use(router);
 
 server.listen(port);
